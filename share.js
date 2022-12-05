1588d42d7a016be914f907f28ae76ebcbc073885
Hansraj <82136657+Hansraj-k@users.noreply.github.com>
2022-12-05 07:24:30 +0530

    Update share.js

--- share.js
+++ share.js
@@ -1,23 +1,19 @@
-/* 
+const shareBtn = document.getElementById('shareBtn')
 
-Social Share Links:
+shareBtn.addEventListener('click', event => {
 
-WhatsApp:
-https://wa.me/?text=[visit our site] [post-url]
-
-*/
-
-const whatsappBtn = document.querySelector(".whatsapp-btn");
-
-function init() {
-  const pinterestImg = document.querySelector(".pinterest-img");
-
-  let postUrl = encodeURI(document.location.href);
-  let postTitle = encodeURI(document.title);
-  whatsappBtn.setAttribute(
-    "href",
-    `https://wa.me/?text=${visit uor site} ${postUrl}`
-  );
-}
-
-init();
+  // Check for Web Share api support
+  if (navigator.share) {
+    // Browser supports native share api
+    navigator.share({
+      text: 'Please read this great article: ',
+      url: 'https://www.google.com/'
+    }).then(() => {
+      console.log('Thanks for sharing!');
+    })
+      .catch((err) => console.error(err));
+  } else {
+    // Fallback
+    alert("The current browser does not support the share function. Please, manually share the link")
+  }
+});
